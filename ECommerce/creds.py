083043b062839c8c2d599a007d1f2314ab03f5f6
Sahitya_Singh <120318995+SinghSahitya@users.noreply.github.com>
2023-05-13 23:05:49 +0530

    Update creds.py

--- ECommerce/creds.py
+++ ECommerce/creds.py
@@ -5,6 +5,3 @@ PASSWORD = "sam07"
 HOST = "127.0.0.1"
 PORT = "5432"
 
-
-MAILGUN_API_KEY = 'key-03014d8fcd3820b627044b58207ef2ea'
-MAILGUN_DOMAIN = 'sandbox9ac137dada9e488b83c11c29e7d4018a.mailgun.org'
